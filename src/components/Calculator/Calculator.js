8be8f9b7223521904d66e90cfa3a3f9469b43e03
Antonio Ferreira <mobileapp@dwcbruma.org>
2025-07-08 20:03:22 +0200

    history done

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -603,7 +603,7 @@ const Calculator = ({ moduleData }) => {
               </div>
 
               <div className="result-row">
-                <div className="result-final">
+                <div className="result-final-primary">
                   {typeof result === 'number' && result > 999999
                     ? result.toLocaleString()
                     : result}
@@ -616,8 +616,8 @@ const Calculator = ({ moduleData }) => {
                 </div>
 
                 {showConversion && (
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <div className="result-final">
+                  <div style={{ display: 'flex', alignItems: 'end', marginLeft: '60px' }}>
+                    <div className="result-final-secondary">
                       {typeof result === 'number'
                         ? (result * exchangeRate).toLocaleString(undefined, {
                             maximumFractionDigits: 2,
@@ -638,10 +638,15 @@ const Calculator = ({ moduleData }) => {
 
           <div className="history-card">
             <div className="history-header">
-              <h3>Calculation History</h3>
-              <button className="clear-history" onClick={clearHistory}>
-                CLEAR HISTORY
-              </button>
+              <h3>
+                Calculation History
+                <button className="clear-history" onClick={clearHistory}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
+                    <path d="M7.48033 3.85444C8.92505 3.43479 10.4684 3.50845 11.8666 4.06381C13.2648 4.61916 14.4381 5.62454 15.2012 6.92112C15.9642 8.2177 16.2735 9.73155 16.0802 11.2235C15.8869 12.7155 15.202 14.1005 14.1337 15.1598C13.0654 16.2191 11.6746 16.8923 10.181 17.0729C8.68749 17.2536 7.17631 16.9315 5.88624 16.1575C4.59617 15.3835 3.60075 14.2018 3.05726 12.7989C2.51376 11.3961 2.45315 9.85213 2.88502 8.41101" stroke="#544DB4"/>
+                    <path d="M9.86399 6.72842L6.89627 3.51333L10.1114 0.545619" stroke="#544DB4"/>
+                  </svg>
+                </button>
+              </h3>
             </div>
 
             {calculationHistory.length === 0 ? (
